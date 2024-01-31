27b258d6a4e42a514bf6ddecda89bd078826bd24
ebb08 <ebb_08@outlook.com>
2024-01-30 20:12:56 -0500

    v1

---
+++ README.md
@@ -0,0 +1,6 @@
+
+## Funcionalidades
+
+- Conteo de clics: El contador realiza un seguimiento en tiempo real del número de clics realizados en la página.
+
+- Reinicio del contador: Se proporciona una opción para reiniciar el contador a cero, permitiendo al usuario comenzar de nuevo.
---
+++ src/App.js
@@ -0,0 +1,36 @@
+
+import './App.css';
+import Boton from './componenetes/Boton';
+import Contador from './componenetes/Contador';
+import { useState } from 'react';
+
+function App() {
+
+  const [numClics, setNumClics] = useState(0); 
+
+  const manejarClic = () => {
+    setNumClics(numClics + 1);
+  }
+
+  const reiniciarContador = () => {
+    setNumClics(0);
+  }
+
+  return (
+    <div className="App">
+      <div className='contenedor-principal'>
+        <Contador numClics={numClics} />
+        <Boton 
+        texto="Clic"
+        esBotonDeClic={true}
+        manejarClic={manejarClic}/>
+        <Boton 
+        texto="Reiniciar"
+        esBotonDeClic={false}
+        manejarClic={reiniciarContador}/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
